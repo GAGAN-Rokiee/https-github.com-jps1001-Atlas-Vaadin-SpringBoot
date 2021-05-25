885763be7879e1d9520590a5e4c5eece9be9f8a3
stalker16 <faisal.khan16@hotmail.com>
2021-05-25 22:31:55 +0300

    workflow api code merge

--- beplatform-designer/src/main/java/it/besolution/BeplatformDesignerApplication.java
+++ beplatform-designer/src/main/java/it/besolution/BeplatformDesignerApplication.java
@@ -2,15 +2,34 @@ package it.besolution;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.page.Push;
+import com.vaadin.flow.component.page.Viewport;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.shared.ui.Transport;
+import com.vaadin.flow.theme.Theme;
+
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-public class BeplatformDesignerApplication {
+@Theme(value = "myapp")
+@PWA(name = "atlas",shortName = "atlas")
+@Viewport("width=device-width, minimum-scale=1.0, initial-scale=1.0")
+@Push(transport = Transport.LONG_POLLING)
+public class BeplatformDesignerApplication implements AppShellConfigurator {
 
-    public static void main(String[] args) {
-        SpringApplication.run(BeplatformDesignerApplication.class, args);
-    }
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args) {
+		 SpringApplication.run(BeplatformDesignerApplication.class, args);
+		}
+
+
+
+    
 }
